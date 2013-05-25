7b21d3bfd7b18cc5833cdc1e352e2961087b49f1
TechFounder <techfounder@outlook.com>
2013-05-25 13:51:05 -0400

    Change happitails and add word_freq

--- happitails.rb
+++ happitails.rb
@@ -1,121 +1,157 @@
-class Clients
+class Shelter
+  attr_accessor :name, :clients, :animals
 
-  attr_accessor :name, :age, :gender, :kids, :pets
-
-  def initialize(name, age, gender, kids, pets)
+  def initialize(name)
     @name = name
-    @age = age
-    @gender = gender
-    @kids = kids
-    @pets = pets
+    @clients = []
+    @animals = []
   end
 
-  def listClients
-    Puts "Here's a list of Clients that uses the shelter: #{name}"
-  end
-end
-
-class Animals
-
-  attr_accessor :name, :breed, :age, :gender, :toy
-
-  def initialize(name, breed, age, gender, toy)
-    @name = name
-    @breed = breed
-    @age = age
-    @gender = gender
-    @toys = toy
+  def add_client(newClient)
+    @clients << newClient
   end
 
-  def listAnimalsYes
-    Puts "Here's a list of Animals available for adoption: #{name}"
+  def add_animal(newAnimal)
+    @animals << newAnimal
   end
 
-  def listAnimalsNo
-    Puts "Here's a list of Animals available for adpotion: #{name}"
+  def delete_animal(deleteAnimal)
+    @animals = delete_animal
   end
-end
 
-john = Clients.new("John", 30, "Male", 2, 0)
-steve = Clients.new("Steve", 50, "Male", 1, 1)
-kate = Clients.new("Kate", 25, "Female", 0, 3)
-newt = Clients.new("Newt", 45, "Female", 4, 5)
-tom = Clients.new("Tom", 18, "Male", 0, 1)
+  def to_animals
+    animals.each do |y|
+      puts "Name: #{y.name}"
+      puts "Kind: #{y.breed}"
+      puts "Age: #{y.age}"
+      puts "Gender: #{y.gender}"
+      puts "Fav Toy: #{y.toy}"
+      puts " "
+    end
+  end
+
+  def to_clients
+    clients.each do |x|
+      puts "Name: #{x.name}"
+      puts "Age: #{x.age}"
+      puts "Gender: #{x.gender}"
+      puts "Kids: #{x.kids}"
+      puts "Pets: #{x.pets}"
+      puts ""
+    end
+  end
+
+  class Client
+
+    attr_accessor :name, :age, :gender, :kids, :pets
+
+    def initialize(name, age, gender, kids, pets)
+      @name = name
+      @age = age
+      @gender = gender
+      @kids = kids
+      @pets = pets
+    end
+  end
+
+  class Animal
+
+    attr_accessor :name, :breed, :age, :gender, :toy
+
+    def initialize(name, breed, age, gender, toy)
+      @name = name
+      @breed = breed
+      @age = age
+      @gender = gender
+      @toy = toy
+    end
+  end
+
+
+  shelter = Shelter.new('HappiTails')
+
+  john = Client.new("John", 30, "Male", 2, 0)
+  steve = Client.new("Steve", 50, "Male", 1, 1)
+  kate = Client.new("Kate", 25, "Female", 0, 3)
+  newt = Client.new("Newt", 45, "Female", 4, 5)
+  tom = Client.new("Tom", 18, "Male", 0, 1)
   clientlist = [john, steve, kate, newt, tom]
 
-gigi = Animals.new("Gigi", "Dog", 8, "Female", "Chewy")
-butch = Animals.new("Butch", "Dog", 3, "Male", "Ball")
-mishka = Animals.new("Mishka", "Cat", 6, "Female", "Box")
-pinky = Animals.new("Pinky", "Cat", 13, "Female", "Sock")
-val = Animals.new("Val", "Snake", 3, "Male", "Hole")
-trex = Animals.new("T-Rex", "Dog", 5, "Male", "GitHub")
+
+  gigi = Animal.new("Gigi", "Dog", 8, "Female", "Chewy")
+  butch = Animal.new("Butch", "Dog", 3, "Male", "Ball")
+  mishka = Animal.new("Mishka", "Cat", 6, "Female", "Box")
+  pinky = Animal.new("Pinky", "Cat", 13, "Female", "Sock")
+  val = Animal.new("Val", "Snake", 3, "Male", "Hole")
+  trex = Animal.new("T-Rex", "Dog", 5, "Male", "GitHub")
   animallist = [gigi, butch, mishka, pinky, val, trex]
 
-puts "(1) List clients"
-puts "(2) List pets"
-puts "(3) Add clients"
-puts "(4) Add pets"
-puts "(5) Remove pets"
-puts ""
-puts "Welcome to HappiTails"
-print "What would you like to do today? "
-resp = gets.chomp.to_i
+  shelter.animals = animallist
+  shelter.clients = clientlist
 
-case resp  
+  shelter.to_animals
+  shelter.to_clients
+
+
+  puts "(1) List clients"
+  puts "(2) List pets"
+  puts "(3) Add clients"
+  puts "(4) Add pets"
+  puts "(5) Remove pets"
+  puts ""
+  puts "Welcome to HappiTails"
+  print "What would you like to do today? "
+  resp = gets.chomp.to_i
+
+  case resp
+
   when 1
-    clientlist.each do |x|
-    puts "Name: #{x.name}"
-    puts "Age: #{x.age}"
-    puts "Gender: #{x.gender}"
-    puts "Kids: #{x.kids}"
-    puts "Pets: #{x.pets}"
-    puts ""
-    end
+    puts client.to_clients
 
   when 2
-    animallist.each do |y|
-    puts "Name: #{y.name}"
-    puts "Kind: #{y.breed}"
-    puts "Age: #{y.age}"
-    puts "Gender: #{y.gender}"
-    puts "Fav Toy: #{y.toy}"
-    puts ""
-    end
+    puts animal.to_animals
+
 
   when 3
     print "Welcome, please enter the following information"
     puts ""
     print "Your first name: "
-    	name = gets.chomp.capitalize
+    name = gets.chomp.capitalize
     print "Your age: "
-    	age = gets.chomp.to_i
+    age = gets.chomp.to_i
     print "Your gender (Male or Female): "
-    	gender = gets.chomp.capitalize
+    gender = gets.chomp.capitalize
     print "How many kids do you have: "
-    	kids = gets.chomp.to_i
+    kids = gets.chomp.to_i
     print "How many pets do you have: "
-    	pets = gets.chomp.to_i
+    pets = gets.chomp.to_i
+
+    client = Client.new(name, age, gender, kids, pets)
+    shelter.add_client(client)
 
   when 4
     print "Welcome, please enter the following information"
     puts ""
     print "Pet name: "
-      name = gets.chomp.capitalize
+    name = gets.chomp.capitalize
     print "Kind of pet: "
-      breed = gets.chomp.capitalize
+    breed = gets.chomp.capitalize
     print "Age of pet: "
-      age = gets.chomp.to_i
+    age = gets.chomp.to_i
     print "Gender of pet: "
-      gender = gets.chomp.capitalize
+    gender = gets.chomp.capitalize
     print "Pet's favorite toy: "
-      toy = gets.chomp.capitalize
+    toy = gets.chomp.capitalize
+
+    animal = Animal.new(name, breed, age, gender, toy)
+    Shelter.add_animal(animal)
 
   when 5
     print "What's the name of the pet that you would like to remove: "
-      remove = gets.chomp.capitalize
-    if remove == pets.name
+    remove = gets.chomp.capitalize
+    Animal.all.each
+    if remove == pets.a_name.delete_animal
 
-    end 
+    end
+  end
 end
-
-
---
+++ word_count.rb
@@ -0,0 +1,8 @@
+class String
+	def unique_word_count
+	end
+
+	def word_frequency_count
+	end
+
+end	
